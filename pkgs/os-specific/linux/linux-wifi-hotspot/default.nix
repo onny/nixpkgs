88dc07e36487b6c911579b51f0691b49366e559c
Jonas Heinrich <onny@project-insanity.org>
2022-04-15 11:18:57 +0200

    fixes

--- pkgs/os-specific/linux/linux-wifi-hotspot/default.nix
+++ pkgs/os-specific/linux/linux-wifi-hotspot/default.nix
@@ -53,7 +53,7 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     wrapProgram $out/bin/create_ap \
-      --prefix PATH : ${lib.makeBinPath [ hostapd  getopt ]}
+      --prefix PATH : ${lib.makeBinPath [ iw hostapd  getopt ]}
 
     wrapProgram $out/bin/wihotspot-gui \
       --prefix PATH : ${lib.makeBinPath [ iw ]} \
