7b224af620f31456a13297b1e540c01798d8164a
oddlama <oddlama@oddlama.org>
2023-07-28 23:20:05 +0200

    immich: init at 1.79.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1852,9 +1852,7 @@ with pkgs;
 
   hyperpotamus = callPackage ../tools/misc/hyperpotamus { };
 
-  immich-cli = callPackage ../tools/misc/immich-cli { };
-
-  inherit (callPackages ../tools/networking/ivpn/default.nix {}) ivpn ivpn-service;
+  inherit (callPackage ../tools/networking/ivpn/default.nix {}) ivpn ivpn-service;
 
   jobber = callPackage ../tools/system/jobber { };
 
@@ -25442,6 +25440,12 @@ with pkgs;
   exhaustive = callPackage ../development/tools/exhaustive { };
 
   hyp = callPackage ../servers/http/hyp { };
+
+  immich = callPackage ../servers/immich {
+    nodejs = nodejs_18;
+    python = python311;
+  };
+  immich-cli = immich.cli;
 
   peering-manager = callPackage ../servers/web-apps/peering-manager { };
 
